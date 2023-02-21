90783815d81eb4eca7671ba9fdedcb27d7744c19
DannickB <bild2707@usherbrooke.ca>
2023-02-21 18:46:44 -0500

    Creation du projet

---
+++ Exo_Control/src/main.cpp
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+#include <Servo.h>
+#include <MegunoLink.h>
+#include "motorControl.h"
+#include "test.h"
+#include "touchScreen.h"
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
