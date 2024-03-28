a90eae4767b2afeedd9c5b13b2d4097a3b81d1ac
Siarlo <simsimtrudeau@gmail.com>
2024-03-28 09:37:56 -0400

    Ameliorer l'exemple

--- ESP-Sensor/exemples/touchScreenExemple.cpp
+++ ESP-Sensor/exemples/touchScreenExemple.cpp
@@ -1,5 +1,7 @@
 #include <Arduino.h>
 
+#include <Nextion.h>
+
 #include "touchScreen.h"
 #include "enumScreenCallbacks.h"
 
@@ -8,6 +10,12 @@ TouchScreen& screen = TouchScreen::getInstance();
 
 void callback(void *ptr){
   Serial.println("Pressed EnumScreenCallback::BUTTON_ARMED");
+
+  // On recoi le ptr vers le boutton en parametre. On peut alors l'utiliser comme ca : 
+  NexButton *bARMED = static_cast<NexButton*>(ptr);
+
+  // Change le text du bouton pour "Pressed" sur l'ecran
+  bARMED->setText("Pressed");
 }
 
 void setup(void) {    
