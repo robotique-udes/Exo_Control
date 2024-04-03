2313336d7f2530e73fd5fb09b0ba6466f1f14a9e
DannickB <bild2707@usherbrooke.ca>
2024-04-03 19:26:37 -0400

    revert main

--- ESP-Sensor/src/main.cpp
+++ ESP-Sensor/src/main.cpp
@@ -1,40 +1,30 @@
-//PROBLEM:
-// Il y a un 3000 de trop dans les lectures en mm
-// Parfois quand on se baisse trop il arrete de faire les lectures
-// Se baisser necessite aussi enormement de courrant pour une raison inconnu
 
 #include <Arduino.h>
-// #include <Servo.h>
-// #include <MegunoLink.h>
 #include "motorControl.h"
 #include "test.h"
 #include <HardwareSerial.h>
 #include "imu.h" 
 #include "touchScreen.h"
 #include <SPI.h>
-//#include <SoftwareSerial.h>
 #include <SD.h>
 #include "imu.h" 
-
 #include <Nextion.h>
 #include <string.h>
 #include <HardwareSerial.h>
-
 #include "enum.h"
-#include "SensorOPT3101.h"
 
-//Motor motor;
-// Test test01;
-//Imu imu01;
-// Screen ecran;
-//Motor* Screen::motor = motor;
+Test tester;
+Relay relais;
+Motor motor;
+Imu imu01;
+Screen ecran;
+Motor* Screen::motor = motor;
 
-// HardwareSerial ESP32Serial1(1);
-// HardwareSerial SerialPort(2);
-// SensorOPT3101 distanceSensor(256);
+HardwareSerial ESP32Serial1(1);
+HardwareSerial SerialPort(2);
 
-//void updateAngles();
-//void sendPWM();
+void updateAngles();
+void sendPWM();
 
   //===============================================================================================================
   //===================================================(SETUP)=====================================================
@@ -42,23 +32,20 @@
 
 void setup()
 {
-  // nexInit();
-  Serial.begin(115200);
-  // ESP32Serial1.begin(9600, SERIAL_8N1, 19, 18);
-  // Serial2.begin(9600, SERIAL_8N1, 16, 17); 
+  relais.setPins();
+  nexInit();
+  Serial.begin(9600);
+  ESP32Serial1.begin(9600, SERIAL_8N1, 19, 18);
+  Serial2.begin(9600, SERIAL_8N1, 16, 17); 
 
 
   Serial.println("Ini motor exo----------");
-  //SensorOPT3101 distanceSensor(256,26,27);
-  // motor.setPins();
-  // imu01.IMUSetup();
-  // motor.setAllRelais(OFF);
+  motor.setPins();
 
-  // distanceSensor.SetTriggerDistance();
+  relais.setAllRelais(OFF);
+  imu01.IMUSetup();
   // imu01.wifiSetup();
 }
-
-
 void loop()
 {
   //--------------Test BLOC----------------
@@ -67,11 +54,9 @@ void loop()
 
 
   //--------------LOGIC BLOC---------------
-  //delay(100);
   //ecran.nextLoop();
-
   //updateAngles(); 
-  // motor.sonarRead(); Ne pas décommenter, remplace par HMI
+  //motor.sonarRead(); //Ne pas décommenter, remplace par HMI
   //motor.neededTorque();
   // motor.neededCurrent(); Ne pas décommenter, pas utile sans PID
   // motor.readCurrent(); Ne pas décommenter, pas utile sans PID
@@ -82,79 +67,67 @@ void loop()
   //Serial.print(motor.getPower());
   //motor.printSonar();
   //motor.printTorque();
-  // Serial.println();
-  // imu01.printAngles();
-  SensorOPT3101 distanceSensor(8,26,27);
-  distanceSensor.SetTriggerDistance();
-
-  while (true){
-    int dist = distanceSensor.GetMinDistance(); // Lib internally prints all 3 sensors
-    Serial.print("\tMinimum disatance : ");
-    Serial.print(dist);
-    Serial.print("  Is on ground : ");
-    Serial.println(distanceSensor.IsOnTheGround());
-  }
-  
+  //imu01.printAngles();
+  Serial.println("");
 }
 
 void updateAngles()
 {
-  //imu01.getAngles();
-  // motor.setAngle(enumIMU::HipR,imu01.getValAngle(enumIMU::HipR));
-  // motor.setAngle(enumIMU::HipL,imu01.getValAngle(enumIMU::HipL));
-  // motor.setAngle(enumIMU::KneeR,imu01.getValAngle(enumIMU::KneeR));
-  // motor.setAngle(enumIMU::KneeL,imu01.getValAngle(enumIMU::KneeL));
+  imu01.getAngles();
+  motor.setAngle(enumIMU::HipR,imu01.getValAngle(enumIMU::HipR));
+  motor.setAngle(enumIMU::HipL,imu01.getValAngle(enumIMU::HipL));
+  motor.setAngle(enumIMU::KneeR,imu01.getValAngle(enumIMU::KneeR));
+  motor.setAngle(enumIMU::KneeL,imu01.getValAngle(enumIMU::KneeL));
 }
 
 void sendPWM()
 {
-  // if (motor.PWMRightKnee > 0)
-  //   motor.PWMRightKnee += 110000; //Rien-In1-In2-EN
-  // else
-  // {
-  //   motor.PWMRightKnee = -motor.PWMRightKnee;
-  //   motor.PWMRightKnee += 101000; //Rien-In1-In2-EN
-  // }
+  if (motor.PWMRightKnee > 0)
+    motor.PWMRightKnee += 110000; //Rien-In1-In2-EN
+  else
+  {
+    motor.PWMRightKnee = -motor.PWMRightKnee;
+    motor.PWMRightKnee += 101000; //Rien-In1-In2-EN
+  }
 
-  // if (motor.PWMLeftKnee > 0)
-  //   motor.PWMLeftKnee += 110000; //Rien-In1-In2-EN
-  // else
-  // {
-  //   motor.PWMLeftKnee = -motor.PWMLeftKnee;
-  //   motor.PWMLeftKnee += 101000; //Rien-In1-In2-EN
-  // }
+  if (motor.PWMLeftKnee > 0)
+    motor.PWMLeftKnee += 110000; //Rien-In1-In2-EN
+  else
+  {
+    motor.PWMLeftKnee = -motor.PWMLeftKnee;
+    motor.PWMLeftKnee += 101000; //Rien-In1-In2-EN
+  }
 
-  // if (motor.PWMRightHip > 0)
-  //   motor.PWMRightHip += 101000; //Rien-In1-In2-EN
-  // else
-  // {
-  //   motor.PWMRightHip = -motor.PWMRightHip;
-  //   motor.PWMRightHip += 110000; //Rien-In1-In2-EN
-  // }
+  if (motor.PWMRightHip > 0)
+    motor.PWMRightHip += 101000; //Rien-In1-In2-EN
+  else
+  {
+    motor.PWMRightHip = -motor.PWMRightHip;
+    motor.PWMRightHip += 110000; //Rien-In1-In2-EN
+  }
 
-  // if (motor.PWMLeftHip > 0)
-  //   motor.PWMLeftHip += 101000; //Rien-In1-In2-EN
-  // else
-  // {
-  //   motor.PWMLeftHip = -motor.PWMLeftHip;
-  //   motor.PWMLeftHip += 110000; //Rien-In1-In2-EN
-  // }
+  if (motor.PWMLeftHip > 0)
+    motor.PWMLeftHip += 101000; //Rien-In1-In2-EN
+  else
+  {
+    motor.PWMLeftHip = -motor.PWMLeftHip;
+    motor.PWMLeftHip += 110000; //Rien-In1-In2-EN
+  }
 
 
-  // std::string msg = std::to_string(motor.PWMRightKnee);
-  // std::string msg2 = std::to_string(motor.PWMLeftKnee);
-  // std::string msg3 = std::to_string(motor.PWMRightHip);
-  // std::string msg4 = std::to_string(motor.PWMLeftHip);
-
+  std::string msg = std::to_string(motor.PWMRightKnee);
+  std::string msg2 = std::to_string(motor.PWMLeftKnee);
+  std::string msg3 = std::to_string(motor.PWMRightHip);
+  std::string msg4 = std::to_string(motor.PWMLeftHip);
   
   
-  // std::string msg5 = msg + msg2 + msg3 + msg4 + "\n";
-  // // ESP32Serial1.write(msg5.c_str());
-  // // Serial.print(msg5.c_str());
-  // motor.PWMRightKnee = 0;
-  // motor.PWMLeftKnee = 0;
-  // motor.PWMRightHip = 0;
-  // motor.PWMLeftHip = 0;
+  std::string msg5 = msg + msg2 + msg3 + msg4 + "\n";
+  ESP32Serial1.write(msg5.c_str());
+  Serial.print(msg5.c_str());
+  motor.PWMRightKnee = 0;
+  motor.PWMLeftKnee = 0;
+  motor.PWMRightHip = 0;
+  motor.PWMLeftHip = 0;
 
 
 }
