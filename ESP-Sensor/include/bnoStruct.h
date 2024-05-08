0400cb6abc540ccaf5d382e38833c5dbecbce276
itsseraphii <itsseraphii@gmail.com>
2024-05-08 08:57:45 -0400

    :construction: WIP MUX

--- ESP-Sensor/include/bnoStruct.h
+++ ESP-Sensor/include/bnoStruct.h
@@ -2,11 +2,11 @@
 
 struct BNOStruct
 {
-    quat:               std::vector<int> v(4); // Stored as W, X, Y, Z
-    euler:              std::vector<int> v(3); // Stored as yaw, pitch, roll as radians
-    acceleration:       std::vector<int> v(3); // Stored as X, Y, Z
-    gyro:               std::vector<int> v(3); // Stored as X, Y, Z
-    mag:                std::vector<int> v(3); // Stored as X, Y, Z
-    lin_acceleration:   std::vector<int> v(3); // Stored as X, Y, Z
-    time:               int;
+    std::vector<int> quat{4}; // Stored as W, X, Y, Z
+    std::vector<int> euler{3}; // Stored as yaw, pitch, roll as radians
+    std::vector<int> acceleration{3}; // Stored as X, Y, Z
+    std::vector<int> gyro{3}; // Stored as X, Y, Z
+    std::vector<int> mag{3}; // Stored as X, Y, Z
+    std::vector<int> lin_acceleration{3}; // Stored as X, Y, Z
+    int time;
 };
--- ESP-Sensor/src/biogeniusImu.cpp
+++ ESP-Sensor/src/biogeniusImu.cpp
@@ -3,7 +3,12 @@
 
 IMU::IMU(){
     this->mux = Multiplex();
-    // Hips will use 0x4A, Knee will use 0x4B, back will also use 0x4B
-    // Left BNOs are on multiplex channel 1, right BNOs are on multiplex channel 2
+    // Hips will use 0x4A, Knee will use 0x4B
+    // Keep this order, position in vector is same as enum value
     BNOs.push_back(new BNO(enumIMU.HipL, 1, &this->mux, 0x4A));
+    BNOs.push_back(new BNO(enumIMU.KneeL, 1, &this->mux, 0x4B));
+    BNOs.push_back(new BNO(enumIMU.HipR, 2, &this->mux, 0x4A));
+    BNOs.push_back(new BNO(enumIMU.KneeR, 2, &this->mux, 0x4B));
+    BNOs.push_back(new BNO(enumIMU.Back, 0, &this->mux, 0x4A));
 }
+
